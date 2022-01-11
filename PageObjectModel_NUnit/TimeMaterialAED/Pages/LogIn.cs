69a784904d53274422fcffdee543c75713ffa2ee
Moises Biason  Intertek <moises.biason@intertek.com>
2022-01-11 08:34:09 +0800

    pushing files for POM_UNit

---
+++ PageObjectModel_NUnit/TimeMaterialAED/Pages/LogIn.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeMaterialAED.Pages
+{
+    class LogIn
+    {
+
+        public void LoginSteps (IWebDriver driver)
+
+        {
+
+            //open chrome browser
+           
+            driver.Manage().Window.Maximize();
+
+
+            //launch turnup portal 
+            driver.Navigate().GoToUrl("http://horse.industryconnect.io/Account/Login?ReturnUrl=%2f");
+
+
+            try
+            {
+
+                //identify username textbox and enter valid username
+                IWebElement usernameTextBox = driver.FindElement(By.Id("UserName"));
+                usernameTextBox.SendKeys("hari");
+
+                //identify the password textbox and enter valid password
+                IWebElement passwordTextBox = driver.FindElement(By.Id("Password"));
+                passwordTextBox.SendKeys("123123");
+
+                //click login button
+                IWebElement loginButton = driver.FindElement(By.XPath("//*[@id='loginForm']/form/div[3]/input[1]"));
+                loginButton.Click();
+            }
+            catch(Exception ex)
+            {
+
+                Assert.Fail("Login Page did not load", ex.Message);
+            }
+
+
+        }
+
+
+
+    }
+}
---
+++ PageObjectModel_NUnit/TimeMaterialAED/Tests/TM_Test.cs
@@ -0,0 +1,70 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+using TimeMaterialAED.Pages;
+using TimeMaterialAED.Utilities;
+
+namespace TimeMaterialAdd
+{
+
+    [TestFixture]
+    class TimeMaterialAED : Driver
+    {
+
+
+        [OneTimeSetUp]
+        public void Login()
+
+        {
+
+             driver = new ChromeDriver(@"C:\Users\moises.biason\Documents\ICTestAnalyst\ICTestAnalyst\TestAutoIntro1");
+
+
+            //LogIn Page object initialization
+            LogIn logIn = new LogIn();
+
+            logIn.LoginSteps(driver);
+
+            //Home Page object initialization
+            Home home = new Home();
+
+            home.GoToTMPage(driver);
+
+        }
+
+        [Test, Order(1), Description("Check if user is able to add record in Time and Material Page.")]
+        public void CreateTM()
+        {
+
+            //TM Page object initialization
+            TM tm = new TM();
+            tm.CreateTM(driver);
+
+        }
+
+        [Test, Order(2), Description("Check if user is able to edit record in Time and Material Page.")]
+        public void EditTM()
+        {
+            TM tm = new TM();
+            tm.EditTM(driver);
+        }
+
+        [Test, Order(3), Description("Check if user is able to delete record in Time and Material Page.")]
+        public void DeleteTM()
+        {
+            TM tm = new TM();
+            tm.DeleteTM(driver);
+        }
+
+        [OneTimeTearDown]
+        public void CloseTestRun()
+        {
+            driver.Quit();
+        }
+
+
+    }
+}
+
